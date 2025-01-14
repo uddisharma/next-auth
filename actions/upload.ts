14fbfa2948e37337776e5c36aae1840213e4602c
shreya <shreya@visions.net.in>
2025-01-14 14:54:43 +0530

    vercel blob

---
+++ actions/upload.ts
@@ -0,0 +1,12 @@
+"use server";
+import { put } from "@vercel/blob";
+
+export const uploadFile = async (formData: FormData) => {
+    const imageFile = formData.get("image") as File;
+
+    const blob = await put(imageFile.name, imageFile, {
+        access: "public",
+    });
+
+    return blob
+}
